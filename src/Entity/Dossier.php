779cbe8d6a98a50ce3b90173a6270fdcb62bfce4
baldmanua <sasha.lysyi.1995@gmail.com>
2020-04-08 05:33:43 +0300

    add fool protection Installed Validator add informative form validation add delete button protection

--- src/Entity/Dossier.php
+++ src/Entity/Dossier.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\DossierRepository")
@@ -19,31 +20,44 @@ class Dossier
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
+     * @Assert\NotBlank(message="Dossier type should not be blank")
+     * @Assert\Length(max="255", maxMessage="Dossier type too long")
      */
     private $type;
 
     /**
      * @ORM\Column(type="string", length=255, name="first_name")
+     * @Assert\NotBlank(message="First name should not be blank")
+     * @Assert\Length(max="255", maxMessage="First name too long")
      */
     private $firstName;
 
     /**
      * @ORM\Column(type="string", length=255, name="second_name")
+     * @Assert\NotBlank(message="Second name should not be blank")
+     * @Assert\Length(max="255", maxMessage="Second name too long")
      */
     private $secondName;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Address should not be blank")
+     * @Assert\Length(max="255", maxMessage="Address too long")
      */
     private $address;
 
     /**
-     * @ORM\Column(type="bigint", name="card_number")
+     * @ORM\Column(type="bigint", length=16, name="card_number")
+     * @Assert\NotBlank(message="Card bumber should not be blank")
+     * @Assert\Length(min="16", max="16", exactMessage="Card bumber should contain 16 digits")
+     * @Assert\Type("digit", message="Card bumber should contain digits only")
      */
     private $cardNumber;
 
     /**
-     * @ORM\Column(type="smallint")
+     * @ORM\Column(type="smallint", length=4)
+     * @Assert\NotBlank(message="CVV Code should not be blank")
+     * @Assert\Length(max="4", maxMessage="The CVV code should be 4 digits max")
      */
     private $cvv;
 
